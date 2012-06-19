2ce0ca0ff983efa80db3797ddac31d5657df9b98
Nick Weil <speedquest@fullsail.edu>
2012-06-19 00:18:12 -0400

    Added Login Button code per the video.  Nice clean lines.

--- Week4/Week4_SingleView/Week4_SingleView/ViewController.m
+++ Week4/Week4_SingleView/Week4_SingleView/ViewController.m
@@ -16,12 +16,21 @@
 
 - (void)viewDidLoad
 {
+    
+    UIButton *loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (loginButton != nil)
+    {
+        loginButton.frame = CGRectMake(190.0f, 70.0f, 100.0f, 30.0f);
+        [loginButton setTitle:@"Login" forState:UIControlStateNormal];
+        [self.view addSubview:loginButton];
+    }
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
+
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
